0c2baf2b7fad3f4d6ad02854934ab18aea413c69
wendianto <wendianto.96@gmail.com>
2015-08-26 21:35:07 +0700

    Update exercise3.cpp
    Yeay !

---
+++ exercise3.cpp
@@ -0,0 +1,13 @@
+//Kamus
+a,b,c : integer;
+
+//Algoritma
+input (a);
+input (b);
+input (c);
+
+if a=b=c then
+output ('SAMA SISI'); 
+else if a=b or a=c or b=c then
+output ('SAMA KAKI');
+else ('SEGITIGA SEMBARANG');
