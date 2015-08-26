affa3d00796e9de95b0c1399290fd69cfcc3a433
wendianto <wendianto.96@gmail.com>
2015-08-26 21:47:17 +0700

    Update exercise4.cpp
    Horaay !!

---
+++ exercise4.cpp
@@ -0,0 +1,18 @@
+//Kamus
+a , b : integer;
+
+//Algoritma
+
+input(a);
+if a = 9999 then
+output (0);
+else
+b <- a
+  repeat
+    input (a);
+      if b > a then 
+      b <- a
+      else
+      b <- b;
+  until (a='9999');
+output (b);
