8a19dbc63c7c8bba97e9abfb16a04011191c14c5
wendianto <wendianto.96@gmail.com>
2015-08-26 21:08:37 +0700

    update excercise1.cpp

---
+++ exercise1.cpp
@@ -0,0 +1,13 @@
+//Kamus
+a,b,c : integer;
+x1,x2,d : real;
+
+//Algoritma
+input(a);
+input(b);
+input(c);
+d < b*b - 4ac
+x1 < (-b + sqrt(d)) / 2a
+x2 < (-b - sqrt(d)) / 2a
+output (x1);
+output (x2);
