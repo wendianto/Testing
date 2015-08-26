ee26399988abfeb154a243d030b4fdbf2e1cd977
wendianto <wendianto.96@gmail.com>
2015-08-26 21:50:40 +0700

    Update exercise3.cpp

--- exercise3.cpp
+++ exercise3.cpp
@@ -7,7 +7,7 @@ input (b);
 input (c);
 
 if a=b=c then
-output ('SAMA SISI'); 
+output ('SAMA SISI')
 else if a=b or a=c or b=c then
-output ('SAMA KAKI');
-else ('SEGITIGA SEMBARANG');
+output ('SAMA KAKI')
+else output ('SEGITIGA SEMBARANG');
